31f398885e3295db49ba4ac7177c25ea9abfbbca
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-12-09 19:59:20 -0500

    node app succesfully asks for at least one user

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ function generateHTML(employees) {
   
     employees.forEach((employee) => {
       html += "<h1>" + employee.name + "</h1>\n";
-      html += "<p>Role: " + employee.role + "</p>\n";
+      html += "<h2>Role: " + employee.role + "</h2>\n";
+      html += "<p>ID: " + employee.id + "</p>\n";
       html += "<p>Email: " + employee.email + "</p>\n";
       html += "<p>Phone: " + employee.phone + "</p>\n";
     });
@@ -58,3 +59,32 @@ function generateHTML(employees) {
       console.log("Employee summary generated successfully!");
     });
   }
+
+  // Prompt the user for information about each employee
+promptUser().then((answers) => {
+  // Add the employee object to the array
+  employees.push(answers);
+
+  // Ask the user if they want to enter another employee
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "continue",
+        message: "Do you want to enter another employee?",
+      },
+    ])
+    .then((response) => {
+      if (response.continue) {
+        // If the user wants to enter another employee, prompt them again
+        promptUser().then((answers) => {
+          // Add the employee object to the array
+          employees.push(answers);
+          // Continue prompting the user until they are finished
+        });
+      } else {
+        // If the user is finished, generate the HTML page
+        generateHTML(employees);
+      }
+    });
+});
