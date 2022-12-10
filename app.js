74fe83c56c60bd3d57e6017caed0bb83d0c9b625
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-12-09 20:16:02 -0500

    added menu functionality but it still throws errors

--- app.js
+++ app.js
@@ -4,33 +4,79 @@ const fs = require("fs");
 // An array to store the employee objects
 const employees = [];
 
-// Prompt the user for information about each employee
-function promptUser() {
+// Prompt the user for information about the team manager
+function promptManager() {
   return inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: "Enter the employee's name:",
+      message: "Enter the team manager's name:",
     },
     {
       type: "input",
-      name: "role",
-      message: "Enter the employee's role:",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Enter the employee's ID:",
+      name: "id",
+      message: "Enter the team manager's employee ID:",
     },
     {
       type: "input",
       name: "email",
-      message: "Enter the employee's email address:",
+      message: "Enter the team manager's email address:",
     },
     {
       type: "input",
-      name: "phone",
-      message: "Enter the employee's phone number:",
+      name: "officeNumber",
+      message: "Enter the team manager's office number:",
+    },
+  ]);
+}
+
+function promptEngineer() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter the engineer's name:",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter the engineer's employee ID:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter the engineer's email address:",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter the engineer's GitHub username:",
+    },
+  ]);
+}
+
+// Prompt the user for information about an intern
+function promptIntern() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter the intern's name:",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter the intern's employee ID:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter the intern's email address:",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Enter the intern's school:",
     },
   ]);
 }
@@ -42,12 +88,17 @@ function generateHTML(employees) {
   
     employees.forEach((employee) => {
       html += "<h1>" + employee.name + "</h1>\n";
-      html += "<h2>Role: " + employee.role + "</h2>\n";
       html += "<p>ID: " + employee.id + "</p>\n";
       html += "<p>Email: " + employee.email + "</p>\n";
-      html += "<p>Phone: " + employee.phone + "</p>\n";
+      if (employee.role === "Manager") {
+        html += "<p>Office number: " + employee.officeNumber + "</p>\n";
+      } else if (employee.role === "Engineer") {
+        html += "<p>GitHub username: " + employee.github + "</p>\n";
+      } else if (employee.role === "Intern") {
+        html += "<p>School: " + employee.school + "</p>\n";
+      }
     });
-  
+
     html += "</body>\n</html>";
   
     // Write the HTML to a file
@@ -60,31 +111,53 @@ function generateHTML(employees) {
     });
   }
 
-  // Prompt the user for information about each employee
-promptUser().then((answers) => {
-  // Add the employee object to the array
-  employees.push(answers);
-
-  // Ask the user if they want to enter another employee
-  inquirer
-    .prompt([
+  function promptUser() {
+    return inquirer.prompt([
       {
-        type: "confirm",
-        name: "continue",
-        message: "Do you want to enter another employee?",
+        type: "list",
+        name: "type",
+        message: "What type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
       },
-    ])
-    .then((response) => {
-      if (response.continue) {
-        // If the user wants to enter another employee, prompt them again
-        promptUser().then((answers) => {
-          // Add the employee object to the array
-          employees.push(answers);
-          // Continue prompting the user until they are finished
-        });
-      } else {
-        // If the user is finished, generate the HTML page
-        generateHTML(employees);
-      }
-    });
+    ]);
+  }
+
+  // Prompt the user for information about the team manager
+  promptManager().then((manager) => {
+  // Add the manager object to the array
+  employees.push(manager);
+  
+
+  // Show the menu
+    return inquirer.prompt([
+      {
+        type: "list",
+        name: "type",
+        message: "What type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+      },
+  ]).then((answer) => {
+    if (answer.type === "Engineer") {
+      // Prompt the user for information about the engineer
+      promptEngineer().then((engineer) => {
+        // Add the engineer object to the array
+        employees.push(engineer);
+
+        // Show the menu again
+        promptUser();
+      });
+    } else if (answer.type === "Intern") {
+      // Prompt the user for information about the intern
+      promptIntern().then((intern) => {
+        // Add the intern object to the array
+        employees.push(intern);
+
+        // Show the menu again
+        promptUser();
+      });
+    } else {
+      // If the user does not want to add any more team members, generate the HTML page
+      generateHTML(employees);
+    }
+  });
 });
