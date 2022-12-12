b3d16287ceca0a6ea187e854cf65d36b6dc03650
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-12-11 20:45:37 -0500

    reconfigured almost all of the code to incorporate objects of employee, manager, engineer, and intern

--- app.js
+++ app.js
@@ -1,186 +1,303 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// An array to store the employee objects
-const employees = [];
+// Create an Employee class
+class Employee {
+  constructor(name, id, email, role = "Employee") {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = role;
+  }
 
-// Show the menu
-function showMenu() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "type",
-      message: "What type of team member would you like to add?",
-      choices: ["Manager", "Engineer", "Intern", "I don't want to add any more team members"],
-    },
-  ]).then((answer) => {
-    if (answer.type === "Manager") {
-      // Prompt the user for information about the team manager
-      promptManager().then((manager) => {
-        // Add the manager object to the array
-        employees.push(manager);
+  getName() {
+    return this.name;
+  }
 
-        // Show the menu again
-        showMenu();
-      });
-    } else if (answer.type === "Engineer") {
-      // Prompt the user for information about the engineer
-      promptEngineer().then((engineer) => {
-        // Add the engineer object to the array
-        employees.push(engineer);
+  getId() {
+    return this.id;
+  }
 
-        // Show the menu again
-        showMenu();
-      });
-    } else if (answer.type === "Intern") {
-      // Prompt the user for information about the intern
-      promptIntern().then((intern) => {
-        // Add the intern object to the array
-        employees.push(intern);
+  getEmail() {
+    return this.email;
+  }
 
-        // Show the menu again
-        showMenu();
-      });
-    } else {
-      // If the user does not want to add any more team members, generate the HTML page
-      generateHTML(employees);
-    }
-  });
+  getRole() {
+    return this.role;
+  }
 }
 
+// Create a Manager class that extends Employee
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email, "Manager");
+    this.officeNumber = officeNumber;
+  }
 
-// Prompt the user for information about the team manager
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+}
+
+// Create an Engineer class that extends Employee
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email, "Engineer");
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
+}
+
+// Create an Intern class that extends Employee
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email, "Intern");
+    this.school = school;
+  }
+
+  getSchool() {
+    return this.school;
+  }
+}
+
+// Create an empty array to hold the team
+const team = [];
+
+// Prompt the user for the team manager's information
 function promptManager() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Enter the team manager's name:",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Enter the team manager's employee ID:",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter the team manager's email address:",
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "Enter the team manager's office number:",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the team manager's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the team manager's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the team manager's office number?",
+      },
+    ])
+    .then((answers) => {
+      // Create a new Manager object
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+
+      // Add the manager to the team
+      team.push(manager);
+
+      // Show the team menu
+      showMenu();
+    });
 }
 
+// Prompt the user for an engineer's information
 function promptEngineer() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Enter the engineer's name:",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Enter the engineer's employee ID:",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter the engineer's email address:",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter the engineer's GitHub username:",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub username?",
+      },
+    ])
+    .then((answers) => {
+      // Create a new Engineer object
+      const engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+
+     // Add the engineer to the team
+     team.push(engineer);
+
+     // Show the team menu
+     showMenu();
+   });
 }
 
-// Prompt the user for information about an intern
+// Prompt the user for an intern's information
 function promptIntern() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Enter the intern's name:",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Enter the intern's employee ID:",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter the intern's email address:",
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "Enter the intern's school:",
-    },
-  ]);
+ inquirer
+   .prompt([
+     {
+       type: "input",
+       name: "name",
+       message: "What is the intern's name?",
+     },
+     {
+       type: "input",
+       name: "id",
+       message: "What is the intern's ID?",
+     },
+     {
+       type: "input",
+       name: "email",
+       message: "What is the intern's email?",
+     },
+     {
+       type: "input",
+       name: "school",
+       message: "What is the intern's school?",
+     },
+   ])
+   .then((answers) => {
+     // Create a new Intern object
+     const intern = new Intern(
+       answers.name,
+       answers.id,
+       answers.email,
+       answers.school
+     );
+
+     // Add the intern to the team
+     team.push(intern);
+
+     // Show the team menu
+     showMenu();
+   });
 }
 
-// Generate an HTML page that displays summaries for each employee
-function generateHTML(employees) {
-  var html = `
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Employee Summary</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-  `;
+// Show the team menu
+function showMenu() {
+ inquirer
+   .prompt([
+     {
+       type: "list",
+       name: "choice",
+       message: "What would you like to do?",
+       choices: [
+         "Add an engineer",
+         "Add an intern",
+         "Finish building the team",
+       ],
+     },
+   ])
+   .then((answers) => {
+     // Add an engineer
+     if (answers.choice === "Add an engineer") {
+       promptEngineer();
+     }
 
-  employees.forEach((employee) => {
-    html += `
-    <div class="card mt-4" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${employee.name}</h5>
-        <p class="card-text"><em>${employee.role}</em></p>
-        <p class="card-text">ID: ${employee.id}</p>
-        <p class="card-text">Email: ${employee.email}</p>
-        `;
-        switch (employee.role) {
-          case "Manager":
-            html += `<p class="card-text">Office number: ${employee.officeNumber}</p>`;
-            break;
-          case "Engineer":
-            html += `<p class="card-text">GitHub username: ${employee.github}</p>`;
-            break;
-          case "Intern":
-            html += `<p class="card-text">School: ${employee.school}</p>`;
-            break;
+     // Add an intern
+     if (answers.choice === "Add an intern") {
+       promptIntern();
+     }
+
+     // Finish building the team
+     if (answers.choice === "Finish building the team") {
+       generateHTML(team);
+     }
+   });
+}
+
+  // Generate an HTML page that displays summaries for each employee
+  function generateHTML(employees) {
+    var html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Employee Summary</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+      <style>
+        body {
+          background-color: lightblue;
         }
-    html += `
-      </div>
-    </div>
-    `;
-  });
+        .card {
+          margin-top: 40px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="text-center mb-3">Employee Summary</h1>
+            <div class="row">
+              ${employees.map((employee) => `
+                <div class="col-md-4">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">${employee.name}</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">${employee.role}</h6>
+                      <p class="card-text">ID: ${employee.id}</p>
+                      <p class="card-text">Email: ${employee.email}</p>
 
-  html += `
-    </div>
-  </body>
+                      // If the employee is a manager, add the office number (using ternary operators from ES6)
+                      ${
+                        employee.officeNumber
+                          ? `<p class="card-text">Office Number: ${employee.officeNumber}</p>`
+                          : ""
+                     }
+                      // If the employee is an engineer, add the GitHub username
+                      ${
+                        employee.github
+                          ? `<p class="card-text">GitHub: ${employee.github}</p>`
+                          : ""
+                      }
+                      // If the employee is an intern, add the school
+                      ${
+                        employee.school
+                          ? `<p class="card-text">School: ${employee.school}</p>`
+                          : ""
+                      }
+                    </div>
+                  </div>
+                </div>
+              `
+              // Join the array of HTML strings into a single string
+                ).join("")}
+
+              // Unfortunate <div> hell 
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
   </html>
   `;
 
   // Write the HTML to a file
-  fs.writeFile("employees.html", html, function (err) {
-    if (err) {
-      return console.log(err);
-    }
-
-    console.log("Employee summary generated successfully!");
+  fs.writeFile("./employees.html", html, function (err) {
+    if (err) throw err;
+    console.log("Saved!");
   });
 }
 
-// Show the menu
-showMenu();
+// Start the program
+promptManager();
+
